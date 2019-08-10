8cbf79c886548f6ad916633576f2b156e92143a7
rbalduwa1 <rohit.balduwa@gmail.com>
2019-08-10 07:38:35 +0530

    JavaCode

---
+++ src/main/java/com/example/ed_javacode/EdJavacodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.ed_javacode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdJavacodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdJavacodeApplication.class, args);
+	}
+
+}
